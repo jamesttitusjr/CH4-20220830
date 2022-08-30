1a31f54540c342e8e012f1352b34862f8f87d7b8
James Titus <jamesttitusjr@outlook.com>
2022-08-30 09:35:20 -0400

    Add project files.

---
+++ CH4 20220830/CH4.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH4_20220830
+// Pg. 131 Functions are like Methods except they're not in a Top Level Statement. 
+// If you're writing multiple same statements something isn't right.
+// Get the program to work first, then clean up by removing what's not needed (ex. duplicates).
+//
